02585442bc57e55b99388f2281a860999e6f50bb
Pedron444 <pedro.souza505@etec.sp.gov.br>
2025-09-02 12:22:34 -0300

    classe carros pronta

---
+++ src/ClasseCarros.java
@@ -0,0 +1,15 @@
+
+public class ClasseCarros extends ClasseAutomotores {
+ 
+	
+	public ClasseCarros (String corPar, String marcaPar, String modeloPar, String tipoPar) {
+		
+		super(corPar, marcaPar, modeloPar, tipoPar);
+		
+		this.corAt = corPar;
+		this.marcaAt = marcaPar;
+		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
+		
+	}
+}
